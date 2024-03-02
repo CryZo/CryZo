9c7e6f73e652de8e5ede00162ce911c4d77b87bd
Christian Haser <32584944+CryZo@users.noreply.github.com>
2024-03-02 03:19:51 +0100

    Fixed path

--- generate-readme.sh
+++ generate-readme.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo '<picture>' > README.md
-echo '  <source media="(prefers-color-scheme: dark)" srcset="github-snake-dark.svg" />' >> README.md
-echo '  <source media="(prefers-color-scheme: light)" srcset="github-snake.svg" />' >> README.md
-echo '  <img alt="github-snake" src="github-snake.svg" />' >> README.md
+echo '  <source media="(prefers-color-scheme: dark)" srcset="dist/github-snake-dark.svg" />' >> README.md
+echo '  <source media="(prefers-color-scheme: light)" srcset="dist/github-snake.svg" />' >> README.md
+echo '  <img alt="github-snake" src="dist/github-snake.svg" />' >> README.md
 echo '</picture>' >> README.md
