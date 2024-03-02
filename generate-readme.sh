05283bc879892988ec05ce40da91c6ebdd1d658c
Christian Haser <32584944+CryZo@users.noreply.github.com>
2024-03-02 03:22:34 +0100

    Updated `generate-readme.sh`

--- generate-readme.sh
+++ generate-readme.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-echo '<picture>' > README.md
+echo '# Welcome to CryZo\'s Profile!' > README.md
+
+echo '<picture>' >> README.md
 echo '  <source media="(prefers-color-scheme: dark)" srcset="dist/github-snake-dark.svg" />' >> README.md
 echo '  <source media="(prefers-color-scheme: light)" srcset="dist/github-snake.svg" />' >> README.md
 echo '  <img alt="github-snake" src="dist/github-snake.svg" />' >> README.md
